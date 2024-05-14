99b7e072be42fbae44f5a6712bf2eb3cb07d4710
NoaDenise <noa.ishac@chasacademy.segit config --global user.email noa.ishac@chasacademy.segit config --global user.email noa.ishac@chasacademy.se>
2024-05-14 21:06:41 +0200

    updated code

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,4 +1,4 @@
-import AboutMe from "../AboutMe";
+import AboutMe from "../AboutMe.jsx";
 import ContactMe from "../ContactMe";
 import Footer from "../Footer";
 import HeroSection from "../HeroSection";
